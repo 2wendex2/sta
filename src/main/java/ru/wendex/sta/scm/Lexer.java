9bd6f15e21ce40ad4c980cb430583a26a0955771
Wendex <2wendex2@mail.ru>
2022-05-16 02:54:34 +0300

    Неработающий лексер для чисел

--- src/main/java/ru/wendex/sta/scm/Lexer.java
+++ src/main/java/ru/wendex/sta/scm/Lexer.java
@@ -12,7 +12,8 @@ public class Lexer {
 	private Position pos;
 	private ArrayList<LexicError> errs = new ArrayList<>();
 	private int lineb, columnb;
-	private StringBuilder numBuilder;
+	private StringBuilder sb;
+	private int sbi;
 	
 	public Lexer(Position pos) throws IOException {
 		this.pos = pos;
@@ -53,181 +54,410 @@ public class Lexer {
 		return isInitial(c) || isDigit(c) || isSpecialSubsequent(c);
 	}
 	
-	private static boolean isErrSubsequent(int c) {
-		return !Character.isWhitespace(c) && c != (int)'(' && c != (int)')' && c != (int)'#' && c != (int)'"' &&
-			c != (int)',' && c != (int)'\'' && c != (int)'`' && c != (int)'.';
+	private static boolean isSeparator(int c) {
+		return Character.isWhitespace(c) || c == (int)'(' || c == (int)')' || c == (int)'"' ||
+			c == (int)',' || c == (int)'\'' || c == (int)'`' || c == (int)';';
 	}
 	
-	private void tokenizeString() throws IOException {
-		boolean badChar = false;
-		StringBuilder sb = new StringBuilder();
+	private void addLexicError() {
+		LexicError le = new LexicError(lineb, columnb, pos.getLine(), pos.getColumn() - 1, sb.toString());
+		errs.add(le);
+	}
+	
+	private void tokenizeStringChar() {
 		int c = pos.peek();
-		sb.append((char)c);
-		pos.next();
-		c = pos.peek();
-		while (c != (int)'"') {
-			if (c == Position.EOF_CHAR) {
-				LexicError le = new LexicError(lineb, columnb, pos.getLine(), pos.getColumn() - 1, sb.toString());
-				errs.add(le);
-				break;
-			}
-			sb.append(Character.toChars(c));
-			if (c == (int)'\\') {
+		sb.appendCodePoint(c);
+		if (c == (int)'\\') {
+			pos.next();
+			int c2 = pos.peek();
+			if (c2 != Position.EOF_CHAR) {
+				sb.appendCodePoint(c2);
 				pos.next();
-				c = pos.peek();
-				if (c != (int)'\\' && c != (int)'"') {
-					badChar = true;
-				}
-				sb.append(Character.toChars(c));
 			}
+		}
+	}
+	
+	private void tokenizeStringSequence() throws IOException {
+		sb = new StringBuilder();
+		int c = pos.peek();
+		sb.appendCodePoint(c);
+		pos.next();
+		for (;;) {
+			c = pos.peek();
+			if (c == Position.EOF_CHAR) {
+				addLexicError();
+				return;
+			}
+			if (c == (int)'"')
+				break;
+			tokenizeStringChar();
+		}
+		sb.appendCodePoint(c);
+		pos.next();
+		sbtokenizeString();
+	}
+	
+	private void sbtokenizeString() {
+		nextToken = new StringToken(Token.STRING, lineb, columnb, pos.getLine(), pos.getColumn()-1, sb.toString());
+	}
+	
+	private void tokenizeSequenceForce() {
+		for (;;) {
+			if (isSeparator(c))
+				break;
+			sb.appendCodePoint(c);
 			pos.next();
 			c = pos.peek();
 		}
-		sb.append((char)c);
-		if (badChar) {
-			LexicError le = new LexicError(lineb, columnb, pos.getLine(), pos.getColumn(), sb.toString());
-			errs.add(le);
-		} else
-			nextToken = new StringToken(Token.STRING_LITERAL, lineb, columnb, pos.getLine(), pos.getColumn(), sb.toString());
-		pos.next();
 	}
 	
-	private void tokenizeIdent() throws IOException {
-		StringBuilder sb = new StringBuilder();
-		for (int c = pos.peek(); isSubsequent(c); c = pos.peek()) {
-			sb.append((char)c);
+	private void tokenizeSequence() {
+		int c0 = pos.peek();
+		pos.next();
+		int c = pos.peek();
+		if (c0 == (int)'#' && c = (int)'(') {
+			nextToken = new Token(Token.VECTOR_PAREN, lineb, columnb, pos.getLine(), pos.getColumn());
 			pos.next();
+			return;
+		}
+		sb = new StringBuilder();
+		sb.appendCodePoint(c0);
+		tokenizeSequenceForce();
+		sbtokenizeSequence();
+	}
+	
+	private void sbtokenizeSequence() {
+		int c0 = sb.codePointAt(0);
+		if (isInitial(c0))
+			sbtokenizeIdent();
+		else if (isDigit(c0))
+			sbtokenizeNumber();
+		else if (c0 == (int)'.') {
+			if (sb.length() == 1)
+				nextToken = new Token(Token.IMPROPER_PERIOD, lineb, columnb, pos.getLine(), pos.getColumn() - 1);
+			else if (sb.length() == 3 && sb.codePointAt(1) == (int)'.' && sb.codePointAt(2) == (int)'.')
+				sbtokenizeIdent();
+			else
+				sbtokenizeNumber();
+		} else if (c0 == (int)'+' || c0 == (int)'-') {
+			if (sb.length() == 1)
+				sbtokenizeIdent();
+			else
+				sbtokenizeNumber();
+		} else if (c0 == (int)'#') {
+			if (sb.length() == 1) {
+				addLexicError();
+				return;
+			}
+			int c1 = sb.codePointAt(1);
+			if (sb.length() == 2) {
+				if (c1 == (int)'f')
+					nextToken = new Token(Token.FALSE_LITERAL, lineb, columnb, pos.getLine(), pos.getColumn() - 1);
+				else if (c1 == (int)'t')
+					nextToken = new Token(Token.TRUE_LITERAL, lineb, columnb, pos.getLine(), pos.getColumn() - 1);
+				else if (c1 == (int)'\\') {
+					if (sb.length() == 2) {
+						int c2 = pos.peek();
+						sb.appendCodePoint(c2);
+						pos.next();
+					}
+					nextToken = new StringToken(Token.CHAR_LITERAL, lineb, columnb, pos.getLine(), pos.getColumn() - 1, sb.toString());
+				} else
+					sbtokenizeNumber();
+			}
+		}
+	}
+	
+	private void sbtokenizeIdent() {
+		int c0 = sb.codePointAt(0);
+		if (!isInitial(c0)) {
+			addLexicError();
+			return;
+		}
+		for (int i = 1; i < sb.length(); i++) {
+			int c = sb.codePointAt(i);
+			if (!isSubsequent(c)) {
+				addLexicError();
+				return;
+			}
 		}
 		nextToken = new StringToken(Token.IDENT, lineb, columnb, pos.getLine(), pos.getColumn()-1, sb.toString().toLowerCase());
 	}
 	
-	private void tokenizePeriod() throws IOException {
-		pos.next();
-		int c = pos.peek();
-		if (c != (int)'.')
-			nextToken = new Token(Token.IMPROPER_PERIOD, lineb, columnb, lineb, columnb);
-		else {
-			pos.next();
-			c = pos.peek();
-			if (c != (int)'.')
-				tokenizeLexicError("..");
-			else {
-				nextToken = new StringToken(Token.IDENT, lineb, columnb, pos.getLine(), pos.getColumn(), "...");
-				pos.next();
-			}
+	
+	
+	
+	private void sbtokenizeNumber() {
+		boolean firstExact;
+		sbi = 0;
+		int c0 = sb.codePointAt(0);
+		if (c0 != (int)'#') {
+			sbtokenizeNumRadix(10);
+			return;
 		}
-	}
-	
-	private void tokenizeComma() throws IOException {
-		pos.next();
-		int c = pos.peek();
-		if (c == (int)'@') {
-			nextToken = new Token(Token.UNQUOTE_SPLICING, lineb, columnb, pos.getLine(), pos.getColumn());
-			pos.next();
-		} else 
-			nextToken = new Token(Token.UNQUOTE, lineb, columnb, lineb, columnb);	
-	}
-	
-	private void tokenizeOcthotorp() throws IOException {
-		pos.next();
-		int c = pos.peek();
-		if (c == (int)'t') {
-			nextToken = new Token(Token.TRUE_LITERAL, lineb, columnb, pos.getLine(), pos.getColumn());
-			pos.next();
-		} else if (c == (int)'f') {
-			nextToken = new Token(Token.FALSE_LITERAL, lineb, columnb, pos.getLine(), pos.getColumn());
-			pos.next();
-		} else if (c == (int)'(') {
-			nextToken = new Token(Token.VECTOR_PAREN, lineb, columnb, pos.getLine(), pos.getColumn());
-			pos.next();
-		} else if (c == (int)'\\') {
-			pos.next();
-			c = pos.peek();
-			if (isSubsequent(c)) {
-				StringBuilder sb = new StringBuilder();
-				for (c = pos.peek(); isSubsequent(c); c = pos.peek()) {
-					sb.append((char)c);
-					pos.next();
-				}
-				
-				String s = sb.toString();
-				if (s.equals("space") || s.equals("newline")) {
-					nextToken = new StringToken(Token.CHAR_LITERAL, lineb, columnb, pos.getLine(), pos.getColumn()-1, "#\\" + s);
-				} else {
-					LexicError le = new LexicError(lineb, columnb, pos.getLine(), pos.getColumn() - 1, "#\\" + s);
-					errs.add(le);
-				}
+		if (sb.length() < 3) {
+			addLexicError();
+			return;
+		}
+		
+		int radix = 10;
+		int c1 = sb.codePointAt(1);
+		if (c1 == (int)'i' || c1 == (int)'e' || c1 == (int)'I' || c1 == (int)'E')
+			firstExact = true;
+		else if (c1 == (int)'b'|| c1 == (int)'B') {
+			firstExact = false;
+			radix = 2;
+		} else if (c1 == (int)'o' || c1 == (int)'O') {
+			firstExact = false;
+			radix = 8;
+		} else if (c1 == (int)'d' || c1 == (int)'D') {
+			firstExact = false;
+		} else if (c1 == (int)'x' || c1 == (int)'X') {
+			firstExact = false;
+			radix = 16;
+		} else {
+			addLexicError();
+			return;
+		}
+		
+		int c2 = sb.codePointAt(2);
+		if (c0 != (int)'#') {
+			sbi = 2;
+			sbtokenizeNumRadix(radix);
+			return;
+		}
+		if (sb.length() < 5) {
+			addLexicError();
+			return;
+		}
+		int c3 = sb.codePointAt(3);
+		if (firstExact) {
+			if (c3 == (int)'b' || c3 == (int)'B') {
+				firstExact = false;
+				radix = 2;
+			} else if (c3 == (int)'o' || c3 == (int)'O') {
+				firstExact = false;
+				radix = 8;
+			} else if (c3 == (int)'d' || c3 == (int)'D') {
+				firstExact = false;
+			} else if (c3 == (int)'x' || c3 == (int)'X') {
+				firstExact = false;
+				radix = 16;
 			} else {
-				nextToken = new StringToken(Token.CHAR_LITERAL, lineb, columnb, pos.getLine(), pos.getColumn()-1, "#\\" + String.valueOf(Character.toChars(c)));
-				pos.next();
+				addLexicError();
+				return;
 			}
-		} else if (c == (int)'b') {
-			pos.next();
-			tokenizePrefNum(2);
-		} else if (c == (int)'o') {
-			pos.next();
-			tokenizePrefNum(8);
-		} else if (c == (int)'d') {
-			pos.next();
-			tokenizePrefNum(10);
-		} else if (c == (int)'x') {
-			pos.next();
-			tokenizePrefNum(16);
-		} else if (c == (int)'i') {
-			pos.next();
-			tokenizePrefExact(false);
-		} else if (c == (int)'e') {
-			pos.next();
-			tokenizePrefExact(true);
-		} else if (isDigit(c)) {
-			tokenizeNum(10, "");
+		} else {
+			if ((c3 != (int)'i' || c3 != (int)'I') && (c3 != (int)'e' || c3 != (int)'E')) {
+				addLexicError();
+				return;
+			}	
 		}
+		sbi = 4;
+		sbtokenizeNumRadix(radix);
+	}
+	
+	
+	private void sbtokenizeNumRadix(int radix) {
+		int c0 = sb.codePointAt(sbi);
+		boolean imaginaryAllowed = false;
+		if (c0 == (int)'+' || c0 == (int)'-') {
+			sbi++;
+			if (sbi >= sb.length()) {
+				addLexicError();
+				return;
+			}
+			int c1 = sb.codePointAt(sbi);
+			if ((c1 == (int)'i' || c1 == (int)'I') && sbi + 1 == sb.length()) {
+				nextToken = new StringToken(Token.NUMBER_LITERAL, lineb, columnb, pos.getLine(), pos.getColumn()-1, sb.toString().toLowerCase());
+				return;
+			}
+			imaginaryAllowed = true;
+		}
+		
+		sbtokenizeUreal(radix);
+		if (sbi == sb.length()) {
+			nextToken = new StringToken(Token.NUMBER_LITERAL, lineb, columnb, pos.getLine(), pos.getColumn()-1, sb.toString().toLowerCase());
+			return;
+		}
+		if (imaginaryAllowed) {
+			int c1 = sb.codePointAt(sbi);
+			if ((c1 == (int)'i' || c1 == (int)'I') && sbi + 1 == sb.length()) {
+				nextToken = new StringToken(Token.NUMBER_LITERAL, lineb, columnb, pos.getLine(), pos.getColumn()-1, sb.toString().toLowerCase());
+				return;
+			}
+		}
+		
+		c0 = sb.codePointAt(sbi);
+		if (c0 != (int)'+' && c0 != (int)'-') {
+			addLexicError();
+			return;
+		}
+		sbi++;
+		if (sbi >= sb.length()) {
+			addLexicError();
+			return;
+		}
+		sbtokenizeUreal(radix);
+		if (sbi >= sb.length()) {
+			addLexicError();
+			return;
+		}
+		
+		int ci = sb.codePointAt(sbi);
+		if ((ci == (int)'i' || ci == (int)'I') && sbi + 1 == sb.length()) {
+			nextToken = new StringToken(Token.NUMBER_LITERAL, lineb, columnb, pos.getLine(), pos.getColumn()-1, sb.toString().toLowerCase());
+			return;
+		}
+		
+		addLexicError();
+	}
+	
+	private void sbtokenizeUreal(int radix) {
+		int c0 = sb.codePointAt(sbi);
+		if (c0 == (int)'.') {
+			sbtokenizeDigitSequence(radix);
+			sbtokenizeOcthotorpSequenceOpt();
+			sbtokenizeSuffix();
+			return;
+		}
+		
+		sbtokenizeDigitSequence(radix);
+		if (sbi == sb.length())
+			return;
+		int c1 = sb.codePointAt(sbi);
+		boolean isOctPeriod = false;
+		if (c1 == (int)'.') {
+			sbi++;
+			sbtokenizeDigitSequenceOpt(radix);
+			sbtokenizeOcthotorpSequenceOpt();
+			sbtokenizeSuffix();
+			return;
+		} else if (c1 == (int)'#') {
+			isOctPeriod = true;
+		}
+		sbtokenizeOcthotorpSequenceOpt();
+		if (sbi == sb.length())
+			return;
+		if (c1 == (int)'.' && isOctPeriod) {
+			sbi++;
+			sbtokenizeOcthotorpSequenceOpt();
+			sbtokenizeSuffix();
+			return;
+		} else if (c1 == (int)'/') {
+			sbi++;
+			if (sbi == sb.length()) {
+				addLexicError();
+				return;
+			}
+			sbtokenizeDigitSequence(radix);
+			sbtokenizeOcthotorpSequenceOpt();
+		}
+		
+		sbtokenizeSuffix();
+	}
+	
+	private void sbtokenizeDigitSequence(int radix) {
+		int c0 = sb.codePointAt(sbi);
+		if (!isRadixDigit(c0, radix)) {
+			addLexicError();
+		}
+		sti++;
+		for (;;) {
+			if (sbi == sb.length())
+				break;
+			int c = sb.codePointAt(sbi);
+			if (!isRadixDigit(c, radix))
+				break;
+			sti++;
+		}
+	}
+	
+	private void sbtokenizeDigitSequenceOpt(int radix) {
+		for (;;) {
+			if (sbi == sb.length())
+				break;
+			int c = sb.codePointAt(sbi);
+			if (!isRadixDigit(c, radix))
+				break;
+			sti++;
+		}
+	}
+	
+	private void sbtokenizeOcthotorpSequenceOpt() {
+		for (;;) {
+			if (sbi == sb.length())
+				break;
+			int c = sb.codePointAt(sbi);
+			if (c != (int)'#')
+				break;
+			sti++;
+		}
+	}
+	
+	private void sbtokenizeSuffix() {
+		if (sbi == sb.length())
+			break;
+		int c = sb.codePointAt(sbi);
+		if (c != (int)'e' || c != (int)'s' || c != (int)'f' || c != (int)'d' || c != (int)'l' ||
+		    c != (int)'E' || c != (int)'S' || c != (int)'F' || c != (int)'D' || c != (int)'L')
+			break;
+		sbi++;
+		if (sbi == sb.length()) {
+			addLexicError();
+			break;
+		}
+		c = sb.codePointAt(sbi);
+		if (c == (int)'+' || c == (int)'-') {
+			sbi++;
+			if (sbi == sb.length()) {
+				addLexicError();
+				break;
+			}
+		}
+		sbtokenizeDigitSequence(10);
+	}
+	
+	private boolean isRadixDigit(int c, int radix) {
+		if (c >= (int)'0' && c < radix + (int)radix && c <= (int)'9')
+			return true;
+		int endLetterUp = (int)'A' + radix - 10;
+		int endLetterDown = (int)'a' + radix - 10;
+		return c >= (int)'A' && c < endLetterUp || c >= (int)'a' && c < endLetterDown;
 	}
 	
 	private void tokenize() throws IOException {
 		lineb = pos.getLine();
 		columnb = pos.getColumn();
 		int c = pos.peek();
-		if (c == (int)'#')
-			tokenizeOcthotorp();
-		else if (c == (int)'(') {
+		if (c == (int)'(') {
 			nextToken = new Token(Token.LPAREN, lineb, columnb, pos.getLine(), pos.getColumn());
 			pos.next();
 		} else if (c == (int)')') {
 			nextToken = new Token(Token.RPAREN, lineb, columnb, pos.getLine(), pos.getColumn());
 			pos.next();
-		} else if (isInitial(c))
-			tokenizeIdent();
-		else if (c == (int)'\'') {
+		} else if (c == (int)'\'') {
 			nextToken = new Token(Token.QUOTE, lineb, columnb, pos.getLine(), pos.getColumn());
 			pos.next();
 		} else if (c == (int)'`') {
 			nextToken = new Token(Token.QUASIQUOTE, lineb, columnb, pos.getLine(), pos.getColumn());
 			pos.next();
-		} else if (c == (int)',')
-			tokenizeComma();
-		else if (c == (int)'.')
-			tokenizePeriod();
-		else if (c == (int)'+')
-			tokenizeSign(true);
-		else if (c == (int)'-')
-			tokenizeSign(false);
+		} else if (c == (int)',') {
+			pos.next();
+			int c2 = pos.peek();
+			if (c2 == (int)'@') {
+				nextToken = new Token(Token.UNQUOTE_SPLICING, lineb, columnb, pos.getLine(), pos.getColumn());
+				pos.next();
+			} else 
+				nextToken = new Token(Token.UNQUOTE, lineb, columnb, lineb, columnb);
+			}
+		}
 		else if (c == (int)'"')
-			tokenizeString();
+			tokenizeStringSequence();
 		else if (c == Position.EOF_CHAR)
 			nextToken = new Token(Token.EOF, lineb, columnb, pos.getLine(), pos.getColumn());
 		else
-			tokenizeLexicError("");
-	}
-	
-	private void tokenizeLexicError(String s) throws IOException {
-		StringBuilder sb = new StringBuilder(s);
-		for (int c = pos.peek(); isErrSubsequent(c); c = pos.peek()) {
-			sb.append(Character.toChars(c));
-			pos.next();
-		}
-		LexicError le = new LexicError(lineb, columnb, pos.getLine(), pos.getColumn() - 1, sb.toString());
-		errs.add(le);
+			tokenizeSequence();
 	}
 				   
 	private void skipWhitespaces() throws IOException {
@@ -252,162 +482,5 @@ public class Lexer {
 			skipWhitespaces();
 			tokenize();
 		}
-	}
-	
-	
-	
-	
-	//Функции для одного префикса (т.е. встретился лишь 1 префикс)
-	private void tokenizePrefExact(boolean exact) {
-		String prefix;
-		if (exact)
-			prefix = "#e";
-		else
-			prefix = "#i";
-		int c = pos.peek();
-		if (c == (int)'#') {
-			pos.next();
-			c = pos.peek();
-			if (c == (int)'b') {
-				pos.next();
-				tokenizeNum(2, prefix + "#b");		
-			} else if (c == (int)'o') {
-				pos.next();
-				tokenizeNum(8, prefix + "#o");
-			} else if (c == (int)'d') {
-				pos.next();
-				tokenizeNum(10, prefix + "#d");
-			} else if (c == (int)'x') {
-				pos.next();
-				tokenizeNum(16, prefix + "#x");
-			} else
-				tokenizeLexicError(prefix + "#");	
-		} else
-			tokenizeNum(10, prefix);
-	}
-	
-	private void tokenizePrefNum(int radix) {
-		String prefix;
-		if (prefix == 2)
-			prefix = "#b";
-		else if (prefix == 8)
-			prefix = "#o";
-		else if (prefix == 10)
-			prefix = "#d";
-		else if (prefix == 16)
-			prefix = "#x";
-		
-		int c = pos.peek();
-		if (c == (int)'#') {
-			pos.next();
-			c = pos.peek();
-			if (c == (int)'e') {
-				pos.next();
-				tokenizeNum(radix, prefix + "#e");		
-			} else if (c == (int)'i') {
-				pos.next();
-				tokenizeNum(radix, prefix + "#i");
-			} else
-				tokenizeLexicError(prefix + "#");	
-		} else
-			tokenizeNum(radix, prefix);
-	}
-	
-	private void tokenizeSign(boolean plus) {
-		String prefix;
-		if (plus)
-			prefix = "+";
-		else
-			prifix = "-";
-		
-	}
-	
-	
-	
-	
-	private boolean isRadixDigit(int c, int radix) {
-		if (c >= (int)'0' && c < radix + (int)radix && c <= (int)'9')
-			return true;
-		int endLetterUp = (int)'A' + radix - 10;
-		int endLetterDown = (int)'a' + radix - 10;
-		return c >= (int)'A' && c < endLetterUp || c >= (int)'a' && c < endLetterDown;
-	}
-	
-	private String tokenizeDigitSequence(int radix) {
-		StringBuilder sb = new StringBuilder();
-		int c = pos.peek();
-		while (isRadixDigit(c, radix)) {
-			sb.append((char)c);
-			pos.next();
-		}
-		return sb.toString();
-	}
-	
-	private String tokenizeOcthotorpSequence() {
-		StringBuilder sb = new StringBuilder();
-		int c = pos.peek();
-		while (c == (int)'#') {
-			sb.append((char)c);
-			pos.next();
-		}
-		return sb.toString();
-	}
-	
-	private boolean tokenizeDigitUreal(int radix) {
-		String ds = tokenizeDigitSequence();
-		String os = tokenizeOcthotorpSequence();
-		
-		int c = pos.peek();
-		if (os.length() == 0 && c == (int)'.') {
-			pos.next();
-			String ds2 = tokenizeDigitSequence();
-			String os2 = tokenizeOcthotorpSequence();
-			numBuilder.append(ds);
-			numBuilder.append(".");
-			numBuilder.append(ds2);
-			numBuilder.append(os2);
-			return tokenizeSuff();
-		} else if (os.length() != 0 && c == (int)'.') {
-			String os2 = tokenizeOcthotorpSequence();
-			numBuilder.append(ds);
-			numBuilder.append(os);
-			numBuilder.append(".");
-			numBuilder.append(os2);
-			return tokenizeSuff();
-		} else if (c == (int)'/') {
-			numBuilder.append(ds);
-			numBuilder.append(os);
-			numBuilder.append("/");
-			String ds2 = tokenizeDigitSequence();
-			String os2 = tokenizeOcthotorpSequence();
-			numBuilder.append(ds2);
-			numBuilder.append(os2);
-			return ds2.length() != 0;
-		} else {
-			numBuilder.append(ds);
-			numBuilder.append(os);
-			return tokenizeSuff();
-		}
-	}
-	
-	private boolean tokenizeSuff() {
-		int c1 = pos.peek();
-		if (c1 != (int)'e' && c1 != (int)'s' && c1 != (int)'f' && c1 != (int)'d' && c1 != (int)'l')
-			return true;
-		numBuilder.append((char)c1);
-		pos.next();
-		int c2 = pos.peek();
-		if (c2 == (int)'+' || c2 == (int)'-') {
-			numBuilder.append((char)c2);
-			pos.next();
-		}
-		
-		String ds = tokenizeDigitSequence(10);
-		if (ds.length() == 0) {
-			return false;
-		}
-			
-		numBuilder.append(ds);
-		return true;
 	}
 }
