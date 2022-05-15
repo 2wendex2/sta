10c089c889df910313f287b79f7ac4078e737139
Wendex <2wendex2@mail.ru>
2022-05-15 04:27:44 +0300

    Начал писать лексер для скима

--- src/main/java/ru/wendex/sta/scm/Lexer.java
+++ src/main/java/ru/wendex/sta/scm/Lexer.java
@@ -3,9 +3,153 @@ package ru.wendex.sta.scm;
 import java.util.ArrayList;
 import java.util.HashMap;
 import ru.wendex.sta.langbase.LexicError;
+import ru.wendex.sta.langbase.Position;
+import ru.wendex.sta.scm.Token;
 
-public abstract class Lexer /*implements ru.wendex.sta.langbase.Lexer*/ {
-	/*Token peek();
-	ArrayList<LexicError> getErrors();
-	void next();*/
+public abstract class Lexer implements ru.wendex.sta.langbase.Lexer {
+	private Token nextToken;
+	private Position pos;
+	private ArrayList<LexicError> errs = new ArrayList<>();
+	private int lineb, columnb;
+	
+	public ru.wendex.sta.langbase.Token peek() {
+		return nextToken
+	};
+	
+	public ArrayList<LexicError> getErrors() {
+		return errs;
+	}
+	
+	private static boolean isLetter(int c) {
+		return c >= (int)'a' && c <= (int)'z' || c >= (int)'A' && c <= (int)'Z';
+	}
+	
+	private static boolean isSpecialInitial(int c) {
+		return c == (int)'!' || c == (int)'$' ||| c == (int)'%' || c == (int)'&' || c == (int)'*' ||
+			c == (int)'/' || c == (int)':' || c == (int)'<' || c == (int)'=' || c == (int)'>' ||
+			c == (int)'?' || c == (int)'^' || c == (int)'_' || c == (int)'~';
+	}
+	
+	private static boolean isInitial(int c) {
+		return isLetter(c) || isSpecialInitial(c);
+	}
+	
+	private static boolean isDigit(int c) {
+		return c >= (int)'0' && c <= (int)'9';
+	}
+	
+	private static boolean isSpecialSubsequent(int c) {
+		return c == (int)'+' || c == (int)'-' ||| c == (int)'.' || c == (int)'@';
+	}
+	
+	private static boolean isSubsequent(int c) {
+		return isInitial(c) || isDigit(c) || isSpecialSubsequent(c);
+	}
+	
+	private static boolean isErrSubsequent(int c) {
+		return !Character.isWhitespace() && c != (int)'(' && c != (int)')' && c != (int)'#' && c != (int)'"' &&
+			c != (int)',' && c != (int)'\'' && c != (int)'`' && c != (int)'.';
+	}
+	
+	private void tokenizeString() {
+		StringBuilder sb = new StringBuilder();
+		int c = pos.peek();
+		sb.append((char)c);
+		pos.next();
+		c = pos.peek();
+		while (c != (int)'"') {
+			if (Position.isNewline(c) || c == Position.EOF_CHAR) {
+				LexicError le = new LexicError(lineb, columnb, pos.getLine(), pos.getColumn() - 1, sb.toString());
+				errs.add(le);
+				break;
+			}
+			sb.append(Character.toChars(c));
+			if (c == (int)'\\') {
+				pos.next();
+				c = pos.peek();
+				if (Position.isNewline(c) || c == Position.EOF_CHAR) {
+					LexicError le = new LexicError(lineb, columnb, pos.getLine(), pos.getColumn() - 1, sb.toString());
+					errs.add(le);
+					break;
+				}
+				sb.append(Character.toChars(c));
+			}
+			pos.next();
+			c = pos.peek();
+		}
+		sb.append((char)c);
+		nextToken = new StringToken(Token.STRING, lineb, columnb, pos.getLine(), pos.getColumn());
+		pos.next();
+	}
+	
+	private void tokenizeIdent() {
+		StringBuilder sb = new StringBuilder();
+		for (int c = pos.peek(); isSubsequent(c); c = pos.peek()) {
+			sb.append((char)c);
+			pos.next();
+		}
+		nextToken = new StringToken(Token.IDENT, lineb, columnb, pos.getLine(), pos.getColumn()-1);
+	}
+	
+	private void tokenize() {
+		lineb = pos.getLine();
+		columnb = posgetColumn();
+		int c = pos.peek();
+		if (c == (int)'#')
+			tokenizeOthotorp();
+		else if (c == (int)'(') {
+			nextToken = new Token(Token.LPAREN, lineb, columnb, pos.getLine(), pos.getColumn());
+			pos.next();
+		} else if (c == (int)')') {
+			nextToken = new Token(Token.RPAREN, lineb, columnb, pos.getLine(), pos.getColumn());
+			pos.next();
+		} else if (isInitial(c))
+			tokenizeIdent();
+		else if (c == (int)'\'') {
+			nextToken = new Token(Token.QUOTE, lineb, columnb, pos.getLine(), pos.getColumn());
+			pos.next();
+		} else if (c == (int)'`') {
+			nextToken = new Token(Token.QUASIQUOTE, lineb, columnb, pos.getLine(), pos.getColumn());
+			pos.next();
+		} else if (c == (int)',')
+			tokenizeComma();
+		else if (c == (int)'.')
+			tokenizePeriod();
+		else if (c == (int)'+' || c == (int)'-')
+			tokenizeSign();
+		else if (c == (int)'"')
+			tokenizeString();
+		else if (c == Position.EOF_CHAR)
+			nextToken = new Token(Token.EOF, lineb, columnb, pos.getLine(), pos.getColumn());
+		else
+			tokenizeLexicError("");
+	}
+	
+	private void tokenizeError(String s) {
+		StringBuilder sb = new StringBuilder(s);
+		for (int c = pos.peek(); isErrSubsequent(c); c = pos.peek()) {
+			sb.append(Character.toChars(c));
+			pos.next();
+		}
+		LexicError le = new LexicError(lineb, columnb, pos.getLine(), pos.getColumn() - 1, sb.toString());
+		errs.add(le);
+	}
+				   
+	private void skipWhitespaces() {
+		for (;;) {
+			int c = pos.peek();
+			if (Character.isWhitespace(c))
+				pos.next();
+			else if (c == (int)';') {
+				pos.next();
+				while (!Position.isNewline(pos.peek()))
+					pos.next();
+			} else
+				break;
+		}
+	}
+	
+	void next() {
+		
+	}
 }
