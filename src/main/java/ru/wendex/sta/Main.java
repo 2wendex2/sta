b2a7d0df381af6f4be0487f58ff7351f11c48d5d
Wendex <2wendex2@mail.ru>
2022-05-30 11:45:35 +0300

    Написал запуск программы

---
+++ src/main/java/ru/wendex/sta/Main.java
@@ -0,0 +1,37 @@
+package ru.wendex.sta;
+
+import ru.wendex.sta.adl.FunctionList;
+import ru.wendex.sta.langbase.Position;
+import ru.wendex.sta.scm.Ast;
+import ru.wendex.sta.scm.Lexer;
+import ru.wendex.sta.scm.Parser;
+import ru.wendex.sta.typea.*;
+
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("usage: <type description file> <scheme source>");
+        }
+        FileReader fr1 = new FileReader(args[0]);
+        Position pos1 = new Position(fr1);
+        Lexer lexer1 = new Lexer(pos1);
+        Ast ast = Parser.parse(lexer1);
+        fr1.close();
+        ScmData scmData = ScmDataBuilder.build(ast);
+
+        FileReader fr2 = new FileReader(args[1]);
+        Position pos2 = new Position(fr2);
+        ru.wendex.sta.adl.Lexer lexer2 = new ru.wendex.sta.adl.Lexer(pos2);
+        FunctionList fl = ru.wendex.sta.adl.Parser.parse(lexer2);
+        fr2.close();
+        TypeaData typeaData = TypeaDataBuilder.build(fl);
+
+        TypeMatcherReport report = AutomataTypeMatcher.match(typeaData, scmData);
+        if (report.getSuccess())
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
